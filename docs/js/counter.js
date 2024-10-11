aa00fcfdd52bfa59a80b4e4fda7940137ec9da25
[Volodymyr Pichkov] <[volodymyr_pichkov@outlook.com]>
2024-10-11 15:18:23 +0300

    let's go

---
+++ docs/js/counter.js
@@ -0,0 +1,22 @@
+export const counter = (function() {
+    let count = 0; // сохранение счетчика
+
+    return function(n) {
+        if (n !== undefined) {
+            count = n; // начало с переданого числа
+        }
+        return count++; // вовзврат текущего значения и его увеличение
+    };
+})();
+
+console.log(counter())
+console.log(counter())
+console.log(counter(100))
+console.log(counter())
+console.log(counter())
+console.log(counter(500))
+console.log(counter())
+console.log(counter())
+console.log(counter(0))
+console.log(counter())
+console.log(counter())
